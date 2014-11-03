5fd6a3af8771037373bf4add9d1362c0c11501b9
Gracer <gracerpro@gmail.com>
2014-11-03 12:52:20 +0300

    Add addon help page

---
+++ protected/views/frontend/site/pages/addon.php
@@ -0,0 +1,4 @@
+<h1>Где скачать аддон для трансфера</h1>
+
+<p>Аддон можно скачать на сайте <a href="http://wowtransfer.com" title="wowtransfer.com">wowtransfer.com</a>
+перейдя по <a href="http://wowtransfer.com/downloads/addon/" title="Скачать аддон для трансфера персонажей WoW">ссылке</a>.</p>
--- protected/views/frontend/site/pages/index.php
+++ protected/views/frontend/site/pages/index.php
@@ -3,15 +3,16 @@
 
 $this->pageTitle = Yii::app()->name . ' - Index';
 $this->breadcrumbs = array(
-	'Index',
+	'Полезная информация',
 );
 ?>
 
 <h1>Полезная информация</h1>
 
 <ul>
-<li>Создание дампа персонажа в 3.3.5a</li>
-<li>Создание дампа персонажа в 4.3.4</li>
+<li><a href="<?php echo $this->createUrl('/site/page/', array('view' => 'addon')) ?>">Где скачать аддон для трансфера?</a></li>
+<li>Создание дампа персонажа в игре 3.3.5a</li>
+<li>Создание дампа персонажа в игре 4.3.4</li>
 <li>Создание заявки на перенос</li>
 <li><a href="<?php echo $this->createUrl('/site/page/', array('view' => 'tconfigs')) ?>">Конфигурации переноса</a></li>
 </ul>
