91b411c192f1228bd587d843f1e755eda43b53e9
Gracer <gracerpro@gmail.com>
2014-11-09 02:14:03 +0300

    Footer to file

---
+++ install/footer.php
@@ -0,0 +1,16 @@
+<div id="footer" class="well well-sm">
+	<div>
+		<a href="http://wowtransfer.com?from=install" title="wowtransfer.com">
+			<img src="images/wowtransfer-icon-48.png" style="float: left;" title="wowtransfer.com" alt="wowtransfer.com">
+		</a>
+
+		Copyright © 2014, <a href="http://wowtransfer.com?from=install" title="wowtransfer.com">wowtransfer.com</a> <span class="label label-info">1.0</span> <br>
+		<span class="glyphicon glyphicon-comment"></span> <a href="http://forum.wowtransfer.com?from=install" title="Форум wowtransfer.com">Форум</a> <br>
+		<!--<a href="http://forum.wowtransfer.com" title="Форум wowtransfer.com">Документация</a>-->
+
+		<img src="images/GitHub-Mark-32px.png" alt="" title="">
+		<span class="glyphicon glyphicon-github"></span> <a href="https://github.com/wowtransfer" title="wowtransfer on github">Сообщество на github</a>,
+		<span class="glyphicon glyphicon-github"></span> <a href="https://github.com/wowtransfer/chdphp" title="wowtransfer on github">chdphp project</a>.
+		<!--<a href="http://api.wowtransfer.com" title="API wowtransfer.com">API</a>-->
+	</div>
+</div>
