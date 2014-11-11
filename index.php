5eed9443e9505980f6cdb4b28818818b56abc486
Gracer <gracerpro@gmail.com>
2014-11-12 00:30:10 +0300

    Hide debug mode on release

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 $installDir = dirname(__FILE__) . '/install';
 if (is_dir($installDir))
 {
-	header('Location: /chdphp/install/');
+	header('Location: /chdphp/install');
 	exit;
 }
 
 $yii = dirname(__FILE__) . '/../../yii/yii.php';
 $config = dirname(__FILE__) . '/protected/config/frontend.php';
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
+if (file_exists('.gitignore'))
+{
+	// remove the following lines when in production mode
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	// specify how many levels of call stack should be shown in each log message
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+}
 
 require_once($yii);
 Yii::createWebApplication($config)->runEnd('frontend');
