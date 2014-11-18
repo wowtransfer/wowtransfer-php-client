679c634343cf97685d914d8e06499a07e4ad061e
Gracer <gracerpro@gmail.com>
2014-11-18 23:45:18 +0300

    Apdate help page

--- protected/views/frontend/site/pages/index.php
+++ protected/views/frontend/site/pages/index.php
@@ -10,9 +10,11 @@ $this->breadcrumbs = array(
 <h1>Полезная информация</h1>
 
 <ul>
-<li><a href="<?php echo $this->createUrl('/site/page/', array('view' => 'addon')) ?>">Где скачать аддон для трансфера?</a></li>
-<li>Создание дампа персонажа в игре 3.3.5a</li>
-<li>Создание дампа персонажа в игре 4.3.4</li>
-<li>Создание заявки на перенос</li>
-<li><a href="<?php echo $this->createUrl('/site/page/', array('view' => 'tconfigs')) ?>">Конфигурации переноса</a></li>
+<li><a href="http://wowtransfer.com/downloads/addon/">Скачать аддон для трансфера</a> с сервиса <a href="http://wowtransfer.com">wowtransfer.com</a>.
+</li>
+<li><a href="http://wowtransfer.com/docs/create-dump-335a/">Создание дампа персонажа в игре 3.3.5a</a> доступно в документации на сервисе.
+</li>
+<li><a href="http://wowtransfer.com/docs/create-dump-434/">Создание дампа персонажа в игре 4.3.4</a> доступно в документаци на сервисе</li>
+<li>Создание заявки на перенос <span class="label label-warning">todo</span></li>
+<li><a href="<?php echo $this->createUrl('/site/page/', array('view' => 'tconfigs')) ?>">Конфигурации переноса</a> <span class="label label-warning">todo</span></li>
 </ul>
