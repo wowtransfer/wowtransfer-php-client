98436b13604c2d210ecd30f07ae757aad35c3458
Vyacheslav <gracerpro@gmail.com>
2015-04-21 23:25:14 +0300

    Fix trinity authenticate

--- protected/components/AccountTrinity.php
+++ protected/components/AccountTrinity.php
@@ -2,23 +2,32 @@
 
 class AccountTrinity extends Account
 {
-	public function authenticate($username, $password)
-	{
-		$command = Yii::app()->db->createCommand('SELECT id, sha_pass_hash, online FROM auth.account WHERE username=:username LIMIT 1');
-		$command->bindValue(':username', $username);
-		$result = $command->queryRow();
-		if ($result === false)
-			return false;
+	/**
+	 * @param string $username
+	 * @param string $password
+	 * @return boolean
+	 */
+	public function authenticate($username, $password) {
+		$command = Yii::app()->db->createCommand();
+		$row = $command
+			->select('id, sha_pass_hash, online')
+			->from('auth.account')
+			->where('username=:username', array(':username' => $username))
+			->limit(1)
+			->queryRow();
 
-		$hash = sha1(strtoupper($username) . ':' . strtoupper($password));
-		if ($hash !== $result['sha_pass_hash'])
-			return false;
+		if ($row) {
+			// The trinity's console write password in uppercase
+			$hash = sha1(strtoupper($username) . ':' . strtoupper($password));
+			if (!strcasecmp($hash, $row['sha_pass_hash'])) {
+				$this->username = $username;
+				$this->password = $password;
+				$this->id       = $row['id'];
+				$this->online   = $row['online'];
+				return true;
+			}
+		}
 
-		$this->username = $username;
-		$this->password = $password;
-		$this->id       = $result['id'];
-		$this->online   = $result['online'];
-
-		return true;
+		return false;
 	}
 }
