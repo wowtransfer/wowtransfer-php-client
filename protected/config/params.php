b924b535110285ce6faf753f1876c0cf90b28be0
Vyacheslav <gracerpro@gmail.com>
2015-05-19 02:04:44 +0300

    Remove app and service config

--- protected/config/params.php
+++ protected/config/params.php
@@ -2,11 +2,26 @@
 
 $params = [];
 
-if (file_exists(__DIR__ . '/app.php')) {
-	$params = array_merge($params, require_once __DIR__ . '/app.php');
+$appFilePath = __DIR__ . '/app.php';
+if (!file_exists($appFilePath)) {
+	$defaultFilePath = __DIR__ . '/app.default.php';
+	$h = fopen($appFilePath, 'w');
+	if ($h) {
+		fwrite($h, file_get_contents($defaultFilePath));
+		fclose($h);
+	}
 }
-if (file_exists(__DIR__ . '/service.php')) {
-	$params = array_merge($params, require_once __DIR__ . '/service.php');
+$params = array_merge($params, require_once $appFilePath);
+
+$serviceFilePath = __DIR__ . '/service.php';
+if (!file_exists($serviceFilePath)) {
+	$defaultFilePath = __DIR__ . '/service.default.php';
+	$h = fopen($serviceFilePath, 'w');
+	if ($h) {
+		fwrite($h, file_get_contents($defaultFilePath));
+		fclose($h);
+	}
 }
+$params = array_merge($params, require_once $serviceFilePath);
 
 return $params;
