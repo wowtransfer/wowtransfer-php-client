5d6b6d49a4595f74909794e0e2bd09eca83761ef
Vyacheslav <gracerpro@gmail.com>
2015-09-05 23:05:47 +0300

    Add simple updater page

--- protected/controllers/backend/UpdatesController.php
+++ protected/controllers/backend/UpdatesController.php
@@ -31,7 +31,14 @@ class UpdatesController extends BackEndController
 
 	public function actionIndex()
 	{
-		$this->render('index');
+		$release = [];
+		$latestReleaseFilePath = $this->getTempReleaseFilePath();
+		if (is_file($latestReleaseFilePath)) {
+			$release['size'] = filesize($latestReleaseFilePath);
+		}
+		$this->render('index', [
+			'release' => $release,
+		]);
 	}
 
 	public function actionLatestRelease() {
@@ -55,5 +62,153 @@ class UpdatesController extends BackEndController
 			}
 		}
 		echo CJSON::encode($result);
+	}
+
+	public function actionUploadRelease() {
+		$request = Yii::app()->request;
+
+		try {
+			$this->uploadArchive();
+		} catch (Exception $ex) {
+			Yii::app()->user->setFlash('error', $ex->getMessage());
+		}
+
+		$this->redirect('index');
+	}
+
+	public function actionDeleteRelease() {
+		$archiveTempFilePath = $this->getTempReleaseFilePath();
+		if (is_file($archiveTempFilePath)) {
+			unlink($archiveTempFilePath);
+		}
+		if (Yii::app()->request->isAjaxRequest) {
+			Yii::app()->end();
+		}
+		$this->redirect('index');
+	}
+
+	public function actionReleaseFiles() {
+		$filePath = $this->getTempReleaseFilePath();
+		if (!is_file($filePath)) {
+			return false;
+		}
+		$response = [];
+		$zip = new ZipArchive();
+		if ($zip->open($filePath)) {
+			for ($i = 0; $i < $zip->numFiles; $i++) {
+				$response[] = $zip->getNameIndex($i);
+			}
+		}
+		header('Content type: application/json; ');
+		echo json_encode($response);
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	protected function uploadArchive() {
+		$file = CUploadedFile::getInstanceByName('archive');
+		if (!$file) {
+			throw new Exception('Файл не загружен');
+		}
+		if (strtolower($file->extensionName) !== 'zip') {
+			throw new Exception('Можно загружать только zip архив');
+		}
+		$archiveTempFilePath = $this->getTempReleaseFilePath();
+		if (!$file->saveAs($archiveTempFilePath)) {
+			throw new Exception('Не удалось скопировать загруженный файл');
+		}
+
+		// from source code: chdphp.zip => dir => target files
+		// from release: chdphp-1.0.zip => chdphp => target files
+
+		$archiveDestDir = self::getReleaseDir();
+		$this->clearDir($archiveDestDir);
+
+		$zip = new ZipArchive();
+		$openResult = $zip->open($archiveTempFilePath);
+		if ($openResult !== true) {
+			throw new Exception('Zip open failed, exit code ' . $openResult);
+		}
+		$zip->extractTo($archiveDestDir);
+		$zip->close();
+
+		$sourceReleaseDir = $this->getSourceReleaseDir($archiveDestDir);
+
+		// overwrite files!!!
+		$fileHelper = new CFileHelper();
+
+		$webRoot = Yii::getPathOfAlias('webroot');
+
+		var_dump($fileHelper->copyDirectory($sourceReleaseDir, $webRoot));
+	}
+
+	/**
+	 * @param string $dir
+	 * @return string
+	 */
+	private function getSourceReleaseDir($dir) {
+		$files = scandir($dir);
+		if ($files && count($files) === 3)  {
+			return $dir . DIRECTORY_SEPARATOR . $files[2];
+		}
+		return $dir;
+	}
+
+	/**
+	 * @param string $dir
+	 * @param string $delete
+	 * @param integer $level
+	 */
+	private function clearDir($dir, $delete = false) {
+		$h = opendir($dir);
+		if (!$h) {
+			return false;
+		}
+		while ($file = readdir($h)) {
+			if ($file !== '.' && $file !== '..') {
+				$path = $dir . DIRECTORY_SEPARATOR . $file;
+				if (is_dir($path)) {
+					$this->clearDir($path, true);
+				}
+				else {
+					unlink($path);
+				}
+			}
+		}
+		closedir($h);
+
+		if ($delete) {
+			rmdir($dir);
+		}
+
+		return true;
+	}
+
+	protected function getTempReleaseFilePath() {
+		return self::getTempReleaseDir() . DIRECTORY_SEPARATOR . 'release.zip';
+	}
+
+	protected function getReleaseFilePath() {
+		return self::getReleaseDir() . DIRECTORY_SEPARATOR . 'release';
+	}
+
+	/**
+	 * @return string
+	 */
+	protected static function getTempReleaseDir() {
+		return Yii::getPathOfAlias('application.runtime');
+	}
+
+	/**
+	 * @return string
+	 */
+	protected static function getReleaseDir() {
+		// TODO: + version, like 1.0.1
+		$dir = Yii::getPathOfAlias('application.runtime.release');
+		if (!is_dir($dir)) {
+			mkdir($dir);
+		}
+		return $dir;
 	}
 }
--- protected/views/backend/updates/index.php
+++ protected/views/backend/updates/index.php
@@ -1,5 +1,6 @@
 <?php
 /* @var $this UpdatesController */
+/* @var $release array */
 
 $this->breadcrumbs = array(
 	'Обновление',
@@ -15,8 +16,8 @@ $this->breadcrumbs = array(
 	<button class="btn btn-default" id="get-lates-version">Проверить</button>
 </div>
 
-<h3>С помощью релиза</h3>
-<div class="alert alert-success">безопасно</div>
+<h3>С помощью последнего релиза <span class="label label-success">безопасно</span></h3>
+
 <ul>
 	<li>Асинхронно выводим информацию о последней версии приложения</li>
 	<li>Скачиваем релиз (zip ~1mb)</li>
@@ -26,6 +27,54 @@ $this->breadcrumbs = array(
 	<li>Удаляем временные файлы</li>
 	<li>Конец</li>
 </ul>
+
+<table>
+	<caption>Релиз</caption>
+	<thead>
+		<tr>
+			<th>Размер</th>
+			<th>Версия</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>
+				<?= isset($release['size']) ? round($release['size'] / 1024 / 1024, 3) : '0' ?> Mb
+			</td>
+			<td>
+				
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<? if (Yii::app()->user->hasFlash('error')): ?>
+<div class="alert alert-danger">
+	<?= Yii::t('app', 'Error') ?>:
+	<?= Yii::app()->user->getFlash('error') ?>
+</div>
+<? endif ?>
+
+<div style="margin-top: 15px;">
+	<?= CHtml::beginForm($this->createUrl('uploadrelease'), 'POST', [
+		'enctype' => 'multipart/form-data',
+	]) ?>
+
+	<?= CHtml::fileField('archive', null, ['id' => 'archive', 'name' => 'archive']) ?>
+	<?= CHtml::submitButton('Загрузить', ['class' => 'btn btn-default']) ?>
+
+	<? if (true): ?>
+	<a href="" id="update-app" class="btn btn-default">Обновить</a>
+	<? endif ?>
+
+	<? if (isset($release['size'])): ?>
+	<a href="<?= $this->createUrl('deleterelease') ?>" class="btn btn-default">
+		<?= Yii::t('app', 'Delete') ?>
+	</a>
+	<? endif ?>
+
+	<?= CHtml::endForm() ?>
+</div>
 
 <h3>С помощью последнего исходного кода</h3>
 <div class="alert alert-danger">опасно, возможны неточности в интерфейсе и проблемы с миграциями.</div>
