1c0e0cb36ee5e8200ef15483c520e9b7ca41b4bd
Vyacheslav <gracerpro@gmail.com>
2016-03-13 10:08:59 +0300

    Disable site url on self

--- public_html/protected/components/FrontendController.php
+++ public_html/protected/components/FrontendController.php
@@ -2,111 +2,114 @@
 
 class FrontEndController extends BaseController
 {
-	/**
-	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
-	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
-	 */
-	public $layout='//layouts/column1';
 
-	/**
-	 * Return menu data
-	 *
-	 * @return array
-	 */
-	public function getMainMenuItems() {
-		$menu = [];
+    /**
+     * @var string the default layout for the controller view. Defaults to '//layouts/column1',
+     * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+     */
+    public $layout = '//layouts/column1';
 
-		$menu[] = ['label' => Yii::t('app', 'Site'), 'url' => Config::getInstance()->getSiteUrl(), 'icon' => 'home'];
-		if (!Yii::app()->user->isGuest) {
-			$menu[] = [
-				'label' => Yii::t('app', 'Requests'), 'url' => ['/transfers'],
-				'icon' => 'list', 'active' => $this->id == 'transfers',
-			];
-		}
-		$menu[] = ['label' => Yii::t('app', 'Help'), 'url' => ['/site/page'], 'icon' => 'info-sign'];
+    /**
+     * Return menu data
+     *
+     * @return array
+     */
+    public function getMainMenuItems()
+    {
+        $menu = [];
 
-		return $menu;
-	}
+        $menu[] = [
+            'label' => Yii::t('app', 'Site'),
+            'url' => Config::getInstance()->getSiteUrl(),
+            'icon' => 'home',
+            'visible' => Config::getInstance()->getSiteUrl() != '/',
+        ];
+        if (!Yii::app()->user->isGuest) {
+            $menu[] = [
+                'label' => Yii::t('app', 'Requests'), 'url' => ['/transfers'],
+                'icon' => 'list', 'active' => $this->id == 'transfers',
+            ];
+        }
+        $menu[] = ['label' => Yii::t('app', 'Help'), 'url' => ['/site/page'], 'icon' => 'info-sign'];
 
-	public function getRightMenuItems() {
-		$lang = Yii::app()->user->lang;
-		$items = [
-			[
-				'label' => 'A',
-				'url' => Yii::app()->request->baseUrl . '/admin.php/transfers/index',
-				'visible' => Yii::app()->user->isAdmin(),
-				'icon' => 'cog',
-				'htmlOptions' => ['title' => Yii::t('app', 'Administration')],
-				'linkOptions' => ['id' => 'to-backend'],
-			],
-			[
-				'label' => '<span class="spr flag-' . $lang . '"></span>',
-				'items' => [
-					[
-						'url' => ['/site/lang', 'lang' => 'ru'],
-						'label' => '<span class="spr flag-ru"></span>',
-						'active' => $lang === 'ru',
-					],
-					[
-						'url' => ['/site/lang', 'lang' => 'en'],
-						'label' => '<span class="spr flag-en"></span>',
-						'active' => $lang === 'en',
-					],
-				]
-			],
-			[
-				'label' => Yii::app()->user->name,
-				'visible' => !Yii::app()->user->isGuest,
-				'items' => [
-					[
-						'url' => ['/site/logout'],
-						'label' => Yii::t('app', 'Logout'),
-						'icon' => 'log-out',
-					],
-				]
-			],
-			[
-				'url' => ['/site/login'],
-				'label' => Yii::t('app', 'Login'),
-				'visible' => Yii::app()->user->isGuest && $this->route !== 'site/login',
-				'icon' => 'log-in',
-			],
-		];
+        return $menu;
+    }
 
-		return $items;
-	}
+    public function getRightMenuItems()
+    {
+        $lang = Yii::app()->user->lang;
+        $items = [
+            [
+                'label' => 'A',
+                'url' => Yii::app()->request->baseUrl . '/admin.php/transfers/index',
+                'visible' => Yii::app()->user->isAdmin(),
+                'icon' => 'cog',
+                'htmlOptions' => ['title' => Yii::t('app', 'Administration')],
+                'linkOptions' => ['id' => 'to-backend'],
+            ],
+            [
+                'label' => '<span class="spr flag-' . $lang . '"></span>',
+                'items' => [
+                    [
+                        'url' => ['/site/lang', 'lang' => 'ru'],
+                        'label' => '<span class="spr flag-ru"></span>',
+                        'active' => $lang === 'ru',
+                    ],
+                    [
+                        'url' => ['/site/lang', 'lang' => 'en'],
+                        'label' => '<span class="spr flag-en"></span>',
+                        'active' => $lang === 'en',
+                    ],
+                ]
+            ],
+            [
+                'label' => Yii::app()->user->name,
+                'visible' => !Yii::app()->user->isGuest,
+                'items' => [
+                    [
+                        'url' => ['/site/logout'],
+                        'label' => Yii::t('app', 'Logout'),
+                        'icon' => 'log-out',
+                    ],
+                ]
+            ],
+            [
+                'url' => ['/site/login'],
+                'label' => Yii::t('app', 'Login'),
+                'visible' => Yii::app()->user->isGuest && $this->route !== 'site/login',
+                'icon' => 'log-in',
+            ],
+        ];
 
-	public function registerCssAndJs() {
-		$cs = Yii::app()->clientScript;
-		$baseUrl = Yii::app()->request->baseUrl;
+        return $items;
+    }
 
-		if (true) { // TODO: minify resource
-			// blueprint CSS framework
-			$cssDir = '/css_dev';
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/main.css', 'screen, projection');
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/print.css', 'print');
-			/*
-			<!--[if lt IE 8]>
-			<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
-			<![endif]-->
-			*/
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/main.css');
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/form.css');
+    public function registerCssAndJs()
+    {
+        $cs = Yii::app()->clientScript;
+        $baseUrl = Yii::app()->request->baseUrl;
 
-			Yii::app()->bootstrap->register();
+        if (true) { // TODO: minify resource
+            $cssDir = '/css_dev';
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/main.css', 'screen, projection');
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/print.css', 'print');
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/main.css');
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/form.css');
 
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/common.css');
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/icons.css');
-			$cs->registerCssFile($baseUrl . $cssDir . '/common/sprite_main.css');
-			$cs->registerCssFile($baseUrl . $cssDir . '/frontend/frontend.css');
+            Yii::app()->bootstrap->register();
 
-			$cs->registerScriptFile($baseUrl . '/js/dev/frontend/main.js', CClientScript::POS_END);
-			$cs->registerScriptFile($baseUrl . '/js/dev/common/common.js', CClientScript::POS_END);
-			$cs->registerScriptFile($baseUrl . '/js/dev/common/dialogs.js', CClientScript::POS_END);
-			$cs->registerScriptFile($baseUrl . '/js/dev/frontend/transfers.js', CClientScript::POS_END);
-		}
-		else {
-			$cs->registerScriptFile($baseUrl . '/js/frontend.min.js', CClientScript::POS_END);
-		}
-	}
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/common.css');
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/icons.css');
+            $cs->registerCssFile($baseUrl . $cssDir . '/common/sprite_main.css');
+            $cs->registerCssFile($baseUrl . $cssDir . '/frontend/frontend.css');
+
+            $cs->registerScriptFile($baseUrl . '/js/dev/frontend/main.js', CClientScript::POS_END);
+            $cs->registerScriptFile($baseUrl . '/js/dev/common/common.js', CClientScript::POS_END);
+            $cs->registerScriptFile($baseUrl . '/js/dev/common/dialogs.js', CClientScript::POS_END);
+            $cs->registerScriptFile($baseUrl . '/js/dev/frontend/transfers.js', CClientScript::POS_END);
+        } else {
+            $cs->registerScriptFile($baseUrl . '/js/frontend.min.js', CClientScript::POS_END);
+        }
+    }
+
 }
