0b4e620b75d0fd2aff34ed968ff81c72623ef741
Vyacheslav <gracerpro@gmail.com>
2016-06-27 01:11:57 +0300

    Move build scripts file

--- public_html/js/buildScripts.js
+++
@@ -1,6 +0,0 @@
-({
-    baseUrl: "../js_dev",
-    mainConfigFile: "../js_dev/frontend/main.js",
-    name: "frontend/main",
-    out: "frontend.min.js"
-})
---
+++ public_html/js_dev/buildScripts.js
@@ -0,0 +1,6 @@
+({
+    baseUrl: ".",
+    mainConfigFile: "frontend/main.js",
+    name: "frontend/main",
+    out: "../js/frontend.min.js"
+})
